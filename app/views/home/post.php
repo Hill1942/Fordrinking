43fb69ffeb8192b020e5a149d2df986becbe1674
Hill1942 <ykdacd123@gmail.com>
2015-01-18 18:17:52 +0800

    change post UI

--- app/views/home/post.php
+++ app/views/home/post.php
@@ -4,31 +4,46 @@
             <img class="post-user-img left" src="<?php echo $data['avatar']; ?>">
         </div>
         <div class="post-action">
-            <div class="post-action-text">
+            <!--<div class="post-action-text">
                 <span id="postTextAngle"></span>
                 <textarea id="postText" class="post-a-text"></textarea>
-            </div>
+            </div>-->
             <div class="post-action-bar">
-                <div class="post-ab-l">
-                    <div class="tab" id="openBlogBtn">
+                <div class="tab post-a-item" id="openBlogBtn">
+                    <div class="post-a-icon">
                         <span class="glyphicon glyphicon-file"></span>
-                        <span class="post-ab-t">Blog</span>
                     </div>
-                    <div class="tab" id="openPictureBtn">
-                        <span class="glyphicon glyphicon-picture"></span>
-                        <span class="post-ab-t">Picture</span>
-                    </div>
-                    <div class="tab" id="openSoundBtn">
-                        <span class="glyphicon glyphicon-headphones"></span>
-                        <span class="post-ab-t">Sound</span>
-                    </div>
-                    <div class="tab" id="openVideoBtn">
-                        <span class="glyphicon glyphicon-facetime-video"></span>
-                        <span class="post-ab-t">Video</span>
-                    </div>
+                    <div class="post-ab-t">Blog</div>
                 </div>
-                <div class="post-ab-r">
-                    <button id="postSubmit">submit</button>
+                <div class="tab post-a-item" id="openPictureBtn">
+                    <div class="post-a-icon">
+                        <span class="glyphicon glyphicon-picture"></span>
+                    </div>
+                    <div class="post-ab-t">Picture</div>
+                </div>
+                <div class="tab post-a-item" id="openSoundBtn">
+                    <div class="post-a-icon">
+                        <span class="glyphicon glyphicon-headphones"></span>
+                    </div>
+                    <div class="post-ab-t">Sound</div>
+                </div>
+                <div class="tab post-a-item" id="openVideoBtn">
+                    <div class="post-a-icon">
+                        <span class="glyphicon glyphicon-facetime-video"></span>
+                    </div>
+                    <div class="post-ab-t">Video</div>
+                </div>
+                <div class="tab post-a-item" id="openMessageBtn">
+                    <div class="post-a-icon">
+                        <span class="glyphicon glyphicon-envelope"></span>
+                    </div>
+                    <div class="post-ab-t">Message</div>
+                </div>
+                <div class="tab post-a-item" id="openPollBtn">
+                    <div class="post-a-icon">
+                        <span class="glyphicon glyphicon-stats"></span>
+                    </div>
+                    <div class="post-ab-t">Poll</div>
                 </div>
             </div>
         </div>
